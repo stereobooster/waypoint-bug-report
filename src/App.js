8d108d64c34d970aaf2f8f1a4dd21ec9e04aab74
stereobooster <stereobooster@gmail.com>
2017-08-11 22:56:35 +0200

    Hack to support scrollspy functionality

--- src/App.js
+++ src/App.js
@@ -6,10 +6,28 @@ import Card from './Card.js';
 const range = (s) => Array.apply(null, Array(s)).map((_, i) => i )
 
 class App extends Component {
+  componentDidMount() {
+    this.currentValue = -1;
+    this.handleScroll = () => {
+      let st = window.pageYOffset || document.documentElement.scrollTop;
+      if (st > this.lastScrollTop){
+        this.scrollDirection = false; //'down';
+      } else {
+        this.scrollDirection = true; //'up';
+      }
+      this.lastScrollTop = st;
+    }
+    window.addEventListener('scroll', this.handleScroll, {passive: true})
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll, {passive: true});
+  }
+
   render() {
     return (
       <div className="App">
-        { range(20).map((i) => <Card key={i} number={i} onEnter={ (x) => console.log(x) } /> ) }
+        { range(20).map((i) => <Card key={i} number={i} onEnter={ (x) => console.log(this.currentValue = this.scrollDirection ? Math.min(x, this.currentValue) : Math.max(x, this.currentValue)) } /> ) }
       </div>
     );
   }
